2ed00280962e0dd7bc0677bec868da4716f529de
ditinagrawal <ditin.agrawal05@gmail.com>
2025-01-22 21:57:04 +0530

    Add custom name feature

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,18 @@
 "use client";
 
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import { GitHubPR } from "@/components/GithubPR";
 
 export default function Home() {
     const searchParams = useSearchParams();
     const name = searchParams.get("name");
+
     return (
         <main>
-            <GitHubPR name={name || "ditinagrawal"} />
+            <Suspense fallback={<div>Loading...</div>}>
+                <GitHubPR name={name || "ditinagrawal"} />
+            </Suspense>
         </main>
     );
 }
