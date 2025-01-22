ce15ddc2dc96d701b832cf91b3cf5cb7d80dae8c
ditinagrawal <ditin.agrawal05@gmail.com>
2025-01-22 22:11:33 +0530

    Fix some bugs

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,9 @@
-"use client";
-
-import { Suspense } from "react";
-import { useSearchParams } from "next/navigation";
 import { GitHubPR } from "@/components/GithubPR";
 
 export default function Home() {
     return (
         <main>
-            <Suspense fallback={<div>Loading...</div>}>
-                <SearchParamsWrapper />
-            </Suspense>
+            <GitHubPR name="ditinagrawal" />
         </main>
     );
-}
-
-function SearchParamsWrapper() {
-    const searchParams = useSearchParams();
-    const name = searchParams.get("name");
-
-    return <GitHubPR name={name || "ditinagrawal"} />;
 }
