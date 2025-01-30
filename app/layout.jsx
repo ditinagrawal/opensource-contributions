9feeb9fe7b4103d569af5001e3ed40a17389df51
ditinagrawal <ditin.agrawal05@gmail.com>
2025-01-30 16:27:21 +0530

    Add Contributors

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning={true}>
             <body className={cn(font.className, "antiliased")}>
                 <ThemeProvider
                     attribute="class"
